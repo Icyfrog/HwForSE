ba686bc7320a76c08d4fd2acae8678db45bedd07
IcyFrog <sjtu-yqc@sjtu.edu.cn>
2018-12-22 14:54:12 +0800

    sssszzZZ

--- learnGit.cpp
+++ learnGit.cpp
@@ -7,5 +7,6 @@ int main(int argc, char const *argv[])
     cout << "hey juggment" << endl;
     cout << "once again";
     cout << "try to quit vim" << endl;
+    cout << "1" << endl;
     return 0;
 }
